92be2221362d75e180401830dad66981c33d9362
CToID <funk443@yahoo.com.tw>
2024-11-07 06:16:45 +0800

    Add a very basic slash command for testing purposes.

---
+++ src/g8_dolphin/features/ping.py
@@ -0,0 +1,34 @@
+# g8-dolphin: a general-purpose Discord bot.
+# Copyright (C) 2024  CToID <funk443@yahoo.com.tw>
+
+# This file is part of g8-dolphin.
+
+# g8-dolphin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# g8-dolphin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with g8-dolphin.  If not, see <https://www.gnu.org/licenses/>.
+
+from discord import Interaction, app_commands
+from discord.ext.commands import Bot, GroupCog
+
+
+class Ping(GroupCog):
+
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+
+    @app_commands.command(name="ping", description="Pong!")
+    async def ping(self, interaction: Interaction) -> None:
+        await interaction.response.send_message("Pong!")
+
+
+async def setup(bot: Bot) -> None:
+    await bot.add_cog(Ping(bot), override=True)
